fae4fa17501c9bca2ea9762cfb451794c23caa71
Patryk Marcinek <patryk.marcinek@elzab.pl>
2021-09-03 21:58:17 +0200

    setup project

---
+++ android/app/src/main/kotlin/pl/com/patryk/garden/MainActivity.kt
@@ -0,0 +1,6 @@
+package pl.com.patryk.garden
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/feature/core/garden_app_widget.dart
@@ -0,0 +1,9 @@
+
+import 'package:flutter/cupertino.dart';
+
+class GardenAppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
