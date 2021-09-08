51d571ba35217cbb4b6dccbca3f51e848c44899e
Patryk Marcinek <patryk.marcinek@elzab.pl>
2021-09-08 22:12:26 +0200

    update bloc provider

--- lib/presentation/core/garden_app_widget.dart
+++ lib/presentation/core/garden_app_widget.dart
@@ -1,26 +1,33 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:garden/generated/locale_keys.g.dart';
-import 'package:garden/presentation/routes/app_router.dart';
-
-
+import 'package:garden/application/plant/plant_form/plant_form_bloc.dart';
+import 'package:garden/application/plant/plant_list/plant_list_bloc.dart';
+import 'package:garden/application/plant/plant_observer/plant_observer_bloc.dart';
+import 'package:garden/injction.dart';
+import 'package:garden/presentation/routes/app_router.gr.dart';
 
 class GardenAppWidget extends StatelessWidget {
-
+  final _appRouter = AppRouter();
   @override
   Widget build(BuildContext context) {
-
     return MultiBlocProvider(
       providers: [
+        BlocProvider<PlantListBloc>(
+          create: (BuildContext context) => getIt(),
+        ),
+        BlocProvider<PlantFormBloc>(
+          create: (BuildContext context) => getIt()..add(const PlantFormEvent.loadPlantTypes()),
+        ),
+        BlocProvider<PlantObserverBloc>(
+          create: (BuildContext context) => getIt(),
+        ),
       ],
-      child: MaterialApp(
-        title: LocaleKeys.app_title,
-        onGenerateRoute: generateRoute,
-        initialRoute: Routes.plant,
+      child: MaterialApp.router(
+        routerDelegate: _appRouter.delegate(),
+        routeInformationParser: _appRouter.defaultRouteParser(),
         theme: ThemeData.dark().copyWith(
-          primaryColor: Colors.yellow,
-          accentColor: Colors.green,
+          primaryColor: Colors.green,
+          accentColor: Colors.lightGreen,
           inputDecorationTheme: InputDecorationTheme(
             border: OutlineInputBorder(
               borderRadius: BorderRadius.circular(8),
